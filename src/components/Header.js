3101757436b2680b6dfcbd69456b420d4fdadf51
JohnnyNWT <jooaaooskt@gmail.com>
2022-10-01 23:15:46 -0300

    Alterando componente Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { userEmail } = this.props;
     return (
-      <div>Header</div>
+      <div>
+        <span data-testid="email-field">
+          Email:
+          { userEmail }
+        </span>
+        <p data-testid="total-field">Despesa Total: 0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </div>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return {
+    userEmail: state.user.email,
+  };
+}
+
+Header.propTypes = {
+  userEmail: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,31 +1,17 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import WalletForm from '../components/WalletForm';
 
 class Wallet extends React.Component {
   render() {
-    const { userEmail } = this.props;
     return (
       <>
         <Header />
-        <span data-testid="email-field">{ userEmail }</span>
-        <span data-testid="total-field">0</span>
-        <span data-testid="header-currency-field">BRL</span>
+        <WalletForm />
         <div>TrybeWallet</div>
       </>
     );
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    userEmail: state.user.email,
-  };
-}
-
-Wallet.propTypes = {
-  userEmail: PropTypes.string.isRequired,
-};
-
-export default connect(mapStateToProps)(Wallet);
+export default Wallet;
