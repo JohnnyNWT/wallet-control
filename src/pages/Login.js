b213b688641cb4202a94da150ab355d6f22b3faa
JohnnyNWT <jooaaooskt@gmail.com>
2023-10-25 19:08:40 -0300

    FEAT: pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { actionUserEmail } from '../redux/actions';
+import WcLogo from '../images/wallet-logo.png';
+import '../css/Login.css';
 
 class Login extends React.Component {
   state = {
@@ -28,31 +30,36 @@ class Login extends React.Component {
     const validEmail = email.toLocaleLowerCase().match(/[a-z]+@[a-z]+.com/);
     const MIN_LENGTH = 6;
     return (
-      <div>
-        <input
-          data-testid="email-input"
-          name="email"
-          type="email"
-          placeholder="email@example.com"
-          onChange={ this.handleChange }
-        />
-        <br />
-        <input
-          data-testid="password-input"
-          name="password"
-          type="password"
-          minLength="6"
-          placeholder="Insira sua senha"
-          onChange={ this.handleChange }
-        />
-        <button
-          data-testid="btn"
-          type="button"
-          disabled={ password.length < MIN_LENGTH || !validEmail }
-          onClick={ this.handleClick }
-        >
-          Entrar
-        </button>
+      <div className="container-login">
+        <div className="container-inputs">
+          <img src={ WcLogo } alt="wallet control logo" width={ 350 } />
+          <input
+            data-testid="email-input"
+            name="email"
+            type="email"
+            placeholder="Email"
+            onChange={ this.handleChange }
+            className="input-login"
+          />
+          <input
+            data-testid="password-input"
+            name="password"
+            type="password"
+            minLength="6"
+            placeholder="Senha"
+            onChange={ this.handleChange }
+            className="input-login"
+          />
+          <button
+            data-testid="btn"
+            type="button"
+            disabled={ password.length < MIN_LENGTH || !validEmail }
+            onClick={ this.handleClick }
+            className="button"
+          >
+            Entrar
+          </button>
+        </div>
       </div>
     );
   }
